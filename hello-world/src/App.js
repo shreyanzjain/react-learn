8fd709789a05f64e3f1c409ad5857f4cc06a3c59
shreyanzjain <shreyans32@outlook.com>
2022-09-23 11:39:14 +0530

    second commit

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -9,6 +9,7 @@ import FunctionClick from "./components/FunctionClick";
 import ClassClick from "./components/ClassClick";
 import EventBind from "./components/EventBind";
 import ParentComponent from "./components/ParentComponent";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
